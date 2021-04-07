7254fc54510b926dd08586705f29d9b85e365c22
Guilherme Fazam <guilherme.fazam@neoris.com>
2021-04-06 21:53:14 -0300

    feat: redirect user authenticated

---
+++ src/components/Login/index.js
@@ -0,0 +1,105 @@
+import {
+    Box,
+    Button,
+    Container,
+    FormControl,
+    FormHelperText,
+    FormLabel,
+    Input,
+    Text,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import Link from "next/link";
+import * as yup from "yup";
+import { Logo } from "../Logo";
+import firebase, { persistenceMode } from "../../config/firebase";
+
+const validationSchema = yup.object().shape({
+    email: yup
+        .string()
+        .email("Email inválido")
+        .required("Preenchimento obrigatório"),
+    password: yup.string().required("Preenchimento obrigatório"),
+});
+
+export const Login = () => {
+    const {
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+    } = useFormik({
+        onSubmit: async (values, form) => {
+            firebase.auth().setPersistence(persistenceMode);
+            try {
+                const user = await firebase
+                    .auth()
+                    .signInWithEmailAndPassword(values.email, values.password);
+                console.log(user);
+            } catch (error) {
+                console.error("ERROR:", error);
+            }
+        },
+        validationSchema,
+        initialValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    return (
+        <Container p={4} centerContent>
+            <Logo />
+            <Box p={4} mt={8}>
+                <Text>Crie sua agenda compartilhada!</Text>
+            </Box>
+            <Box>
+                <FormControl p={4} id="email" isRequired>
+                    <FormLabel>Email</FormLabel>
+                    <Input
+                        size="lg"
+                        type="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                    {touched.email && (
+                        <FormHelperText textColor="#e74c3c">
+                            {errors.email}
+                        </FormHelperText>
+                    )}
+                </FormControl>
+
+                <FormControl p={4} id="password" isRequired>
+                    <FormLabel>Senha</FormLabel>
+                    <Input
+                        size="lg"
+                        type="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                    {touched.password && (
+                        <FormHelperText textColor="#e74c3c">
+                            {errors.password}
+                        </FormHelperText>
+                    )}
+                </FormControl>
+                <Box p={4}>
+                    <Button
+                        colorScheme="blue"
+                        onClick={handleSubmit}
+                        isLoading={isSubmitting}
+                        width="100%"
+                    >
+                        Entrar
+                    </Button>
+                </Box>
+            </Box>
+            <Link href="/signup">Não tem conta? Cadastre-se aqui!</Link>
+        </Container>
+    );
+};
---
+++ src/components/Schedule/index.js
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+import firebase from "../../config/firebase";
+export const Schedule = () => {
+    const logout = () => firebase.auth().signOut();
+
+    return (
+        <div>
+            <Button onClick={logout}>Sair</Button>
+        </div>
+    );
+};
